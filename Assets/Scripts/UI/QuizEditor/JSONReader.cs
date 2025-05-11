24f174b6623c537b9748f13e1f401dc2ff1ae480
learntheba6 <kaizen.yomyom@gmail.com>
2025-05-11 17:53:35 +0800

    Added Saved Input Text on Placeholders in Editor

--- Assets/Scripts/UI/QuizEditor/JSONReader.cs
+++ Assets/Scripts/UI/QuizEditor/JSONReader.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 public class JSONReader : MonoBehaviour
 {
     [SerializeField] private TextAsset jsonData;
+    private QuizList collection;
     public QuizItem[] questions;
+    
     
     void Start()
     {
+        RefreshJson();
+    }
+
+    public void RefreshJson()
+    {
         // Deserialize JSON into a QuizList object.
-        QuizList collection = JsonUtility.FromJson<QuizList>(jsonData.text);
+        collection = JsonUtility.FromJson<QuizList>(jsonData.text);
 
         // Extract the quiz items.
         questions = collection.questions;
@@ -25,7 +32,7 @@ public class JSONReader : MonoBehaviour
                 quiz.choices = new string[] { quiz.answerInput, quiz.filler1Input, quiz.filler2Input };
                 quiz.correct_answer = Array.IndexOf(quiz.choices, quiz.answerInput);
             }
-            
+
             // Output to Unity's Console for verification.
             Debug.Log("Question Number: " + quiz.questionNumber);
             Debug.Log("Question: " + quiz.questionInput);
